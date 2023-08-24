ee0adb546534b3b11efccc4ed014b9d87e961a8b
Sarah Madeline <e.sarah.madeline@gmail.com>
2023-08-24 15:40:07 -0700

    Edit js file

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-// JavaScript code
-const bound = document.getElementById('bound-one');
-const video = bound.querySelector('video');
+const registerVideo = (bound, video) => {
+	bound = document.querySelector(bound);
+	video = document.querySelector(video);
+	const scrollVideo = ()=>{
+		if(video.duration) {
+			const distanceFromTop = window.scrollY + bound.getBoundingClientRect().top;
+			const rawPercentScrolled = (window.scrollY - distanceFromTop) / (bound.scrollHeight - window.innerHeight);
+			const percentScrolled = Math.min(Math.max(rawPercentScrolled, 0), 1);
+			
+			video.currentTime = video.duration * percentScrolled;
+		}
+		requestAnimationFrame(scrollVideo);
+	};
+	requestAnimationFrame(scrollVideo);
+};
 
-window.addEventListener('scroll', () => {
-    const scrollPosition = window.scrollY;
-    video.style.transform = `translateY(-${scrollPosition * 0.8}px)`; // Adjust the factor for parallax effect
-});
+
+registerVideo("#bound-one", "#bound-one video");
+
