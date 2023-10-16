d124a666046321ad205dac81df91b38b9e38bd47
Sarah Madeline <e.sarah.madeline@gmail.com>
2023-10-16 15:27:38 -0700

    Edit setInterval speed from 20 to 30 to slow image

--- script.js
+++ script.js
@@ -1,57 +1,58 @@
-let img = document.getElementById("interactive-img");
-let imgWidth = img.offsetWidth;
-let movingRight = true;
-let isDragging = false;
-
-// Update the image width dynamically in case of window resizing
-window.addEventListener("resize", () => {
-    imgWidth = img.offsetWidth;
-});
-
-// Move the image automatically
-setInterval(() => {
-  if (!isDragging) {
-    let position = parseInt(window.getComputedStyle(img).left, 10);
-    if (movingRight) {
-      position += 1;
-    } else {
-      position -= 1;
-    }
-
-    // Keep image within bounds
-    if (position >= window.innerWidth - imgWidth) {
-        movingRight = false;
-        position = window.innerWidth - imgWidth;  // Correct the position
-    }
-      if (position <= 0) {
-        movingRight = true;
-        position = 0;  // Correct the position
-    }
+// Function to handle each image
+function handleImage(img) {
+    let imgWidth = img.offsetWidth;
+    let movingRight = true;
+    let isDragging = false;
   
-      img.style.left = position + "px";
-    }
-}, 20);
-
-// Make the image draggable
-img.addEventListener("mousedown", (e) => {
-  isDragging = true;
-  img.style.cursor = "grabbing";
-
-  let offsetX = e.clientX - parseInt(window.getComputedStyle(img).left, 10);
-  let offsetY = e.clientY - parseInt(window.getComputedStyle(img).top, 10);
-
-  document.addEventListener("mousemove", onMouseMove);
-  document.addEventListener("mouseup", onMouseUp);
-
-  function onMouseMove(e) {
-    img.style.left = (e.clientX - offsetX) + "px";
-    img.style.top = (e.clientY - offsetY) + "px";
+    // Move the image automatically
+    setInterval(() => {
+      if (!isDragging) {
+        let position = parseInt(window.getComputedStyle(img).left, 10);
+        if (movingRight) {
+          position += 1;
+        } else {
+          position -= 1;
+        }
+  
+        if (position >= window.innerWidth - imgWidth) {
+          movingRight = false;
+          position = window.innerWidth - imgWidth;
+        }
+        if (position <= 0) {
+          movingRight = true;
+          position = 0;
+        }
+  
+        img.style.left = position + "px";
+      }
+    }, 30);
+  
+    // Make the image draggable
+    img.addEventListener("mousedown", (e) => {
+      isDragging = true;
+      img.style.cursor = "grabbing";
+  
+      let offsetX = e.clientX - parseInt(window.getComputedStyle(img).left, 10);
+      let offsetY = e.clientY - parseInt(window.getComputedStyle(img).top, 10);
+  
+      document.addEventListener("mousemove", onMouseMove);
+      document.addEventListener("mouseup", onMouseUp);
+  
+      function onMouseMove(e) {
+        img.style.left = (e.clientX - offsetX) + "px";
+        img.style.top = (e.clientY - offsetY) + "px";
+      }
+  
+      function onMouseUp() {
+        isDragging = false;
+        img.style.cursor = "grab";
+        document.removeEventListener("mousemove", onMouseMove);
+        document.removeEventListener("mouseup", onMouseUp);
+      }
+    });
   }
-
-  function onMouseUp() {
-    isDragging = false;
-    img.style.cursor = "grab";
-    document.removeEventListener("mousemove", onMouseMove);
-    document.removeEventListener("mouseup", onMouseUp);
-  }
-});
+  
+  // Get all interactive images and apply the function to each of them
+  const images = document.querySelectorAll(".interactive-img");
+  images.forEach(handleImage);
+  
