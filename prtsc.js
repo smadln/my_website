806462ba027fdd14b2b099f1f257591f1f98eaf7
Sarah Madeline <e.sarah.madeline@gmail.com>
2023-11-21 16:41:53 -0800

    Add ascii_green.png to html css javascript files

--- prtsc.js
+++ prtsc.js
@@ -36,6 +36,6 @@ function handleImage(img) {
     
 // Get all interactive images and apply the function to each of them
 window.addEventListener('DOMContentLoaded', (event) => {
-    const images = document.querySelectorAll(".ascii-plant-1 img, .ascii-plant-2 img, .ascii-plant-3 img");
+    const images = document.querySelectorAll(".ascii-plant-1 img, .ascii-plant-2 img, .ascii-plant-3 img, .ascii-plant-4 img");
     images.forEach(handleImage);
 });
