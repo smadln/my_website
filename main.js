cdbf496584dbb94f7a0aabd718a77dd08993e334
Sarah Madeline <e.sarah.madeline@gmail.com>
2024-06-23 16:33:20 -0700

    Add sky with sun position to js

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 import * as THREE from 'three';
 import { Sky } from 'three/examples/jsm/objects/Sky.js';
+
+const sky = new Sky();
+sky.scale.setScalar( 450000 );
+
+const phi = MathUtils.degToRad( 90 );
+const theta = MathUtils.degToRad( 180 );
+const sunPosition = new Vector3().setFromSphericalCoords( 1, phi, theta );
+
+sky.material.uniforms.sunPosition.value = sunPosition;
+
+scene.add( sky );
